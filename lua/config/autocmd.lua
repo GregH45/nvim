5b1d2a1ec7ce322820cdaabbd181a49dc99bbf54
Greg <greg@jamespot.com>
2025-04-16 08:56:29 +0200

    Remove useless autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -13,10 +13,3 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.expandtab = true
   end,
 })
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'neo-tree',
-  callback = function()
-    vim.opt_local.winbar = nil
-    vim.opt_local.statusline = nil
-  end,
-})
